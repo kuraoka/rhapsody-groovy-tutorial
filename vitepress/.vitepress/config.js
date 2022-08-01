d0ed2da2977546b1e60065a27a57feb51466b40a
Mikio Kuraoka <mikio.kuraoka@gmail.com>
2022-08-02 08:45:43 +0900

    Add descriptions in Japanese.

--- vitepress/.vitepress/config.js
+++ vitepress/.vitepress/config.js
@@ -3,12 +3,14 @@ export default {
   description: 'Tutorial for making Rhapsody Plugin.',
   outDir: "../docs/",
   base: "/rhapsody-plugin-tutorial/",
+  ignoreDeadLinks: true,
   themeConfig: {
     sidebar: [{
       items: [
         {text: "Java", link: '/src/java/'},
         {text: "Grovvy", link: '/src/groovy/'},
-        {text: "Scala", link: '/src/scala/'}
+        {text: "Scala", link: '/src/scala/'},
+        {text: "IBM Engineering Information", link: '/ibm-engineering-info/'}
       ]
     }] 
   }
